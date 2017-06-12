d458bc275db77072fb43523f090aabe8e085b330
Kyle Dencker <kdencker@gmail.com>
2017-06-12 10:59:01 -0400

    Day 1 Lecture

---
+++ Android/2017/SpaceGame/html/src/com/c4cheats/spacegame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.c4cheats.spacegame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.c4cheats.spacegame.MainController;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MainController();
+        }
+}
