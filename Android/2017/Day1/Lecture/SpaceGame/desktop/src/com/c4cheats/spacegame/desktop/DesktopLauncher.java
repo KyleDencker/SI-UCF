99855cd5a09e1f6bf79955c839111f67a7d6c3d2
Kyle Dencker <kdencker@gmail.com>
2017-06-12 13:16:27 -0400

    Add files via upload

---
+++ Android/2017/Day1/Lecture/SpaceGame/desktop/src/com/c4cheats/spacegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.c4cheats.spacegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.c4cheats.spacegame.GameController;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameController(), config);
+	}
+}
