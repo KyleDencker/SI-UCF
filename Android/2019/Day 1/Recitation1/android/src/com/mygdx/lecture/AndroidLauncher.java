5e9048c822708a63809158e49e1faf052011ffe6
rjhidalgo <36284939+rjhidalgo@users.noreply.github.com>
2019-06-10 21:54:32 -0400

    Add files via upload

---
+++ Android/2019/Day 1/Recitation1/android/src/com/mygdx/lecture/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.lecture;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.lecture.DungeonLecture;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DungeonLecture(), config);
+	}
+}
---
+++ Android/2019/Day 1/Recitation1/desktop/src/com/mygdx/lecture/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.lecture.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.lecture.DungeonLecture;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1920/2;
+		config.height = 1080/2;
+		new LwjglApplication(new DungeonLecture(), config);
+	}
+}
