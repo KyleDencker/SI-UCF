f93e13ef0a4d794713f4c2e4bfb82600c7bf06bc
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2021-06-14 14:54:08 -0400

    Add files via upload

---
+++ Android/2021/Day 6/Recitation/Clamped Camera follows the Player/Player.java
@@ -0,0 +1,136 @@
+package com.libgdxprojects.learning;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+public class Player
+{
+    Texture texture;
+    Sprite sprite;
+
+    Rectangle hitBox;
+
+    int movementSpeed = 50;
+
+    public float velX = 0, velY = 0;
+
+    int row, col, offset;
+    float timer;
+
+    Array<RectangleMapObject> array;
+
+
+    public Player(int x, int y, Array<RectangleMapObject> arr)
+    {
+        texture = new Texture("playerSprite.png");
+        sprite = new Sprite(texture);
+
+        sprite.setRegion(0, 0, 64, 64);
+
+        sprite.setSize(24,24);
+        sprite.setPosition(x, y);
+
+        hitBox = new Rectangle(x, y, sprite.getWidth(), sprite.getHeight());
+
+        array = arr;
+
+    }
+
+    public void update(float deltaTime)
+    {
+        if (velX > 0)
+        {
+            row = 2;
+            offset = 6;
+        }
+
+        if (velX < 0)
+        {
+            row = 1;
+            offset = 5;
+        }
+
+        if (velY < 0)
+        {
+            row = 0;
+            offset = 0;
+
+        }
+
+        if (velY > 0)
+        {
+            row = 3;
+            offset = 10;
+        }
+
+        if (velX != 0 || velY != 0)
+        {
+            timer += deltaTime;
+
+            if (timer >= 0.1f)
+            {
+                timer = 0;
+                col++;
+
+                if (col == 4)
+                {
+                    col = 0;
+                }
+
+                sprite.setRegion(col * 64, row * 64 + offset, 64, 64);
+            }
+        }
+
+        float oldX = sprite.getX();
+        float oldY = sprite.getY();
+
+        sprite.setPosition(sprite.getX() + velX * deltaTime, sprite.getY() + velY * deltaTime);
+        hitBox.setPosition(sprite.getX() + velX * deltaTime, sprite.getY() + velY * deltaTime);
+
+        for (RectangleMapObject rectangleObject : array)
+        {
+            Rectangle rectangle = rectangleObject.getRectangle();
+
+            if (rectangle.overlaps(hitBox))
+            {
+                sprite.setPosition(oldX, oldY);
+                hitBox.setPosition(oldX, oldY);
+            }
+        }
+
+    }
+
+    public Vector2 getPosition()
+    {
+        return new Vector2(sprite.getX(), sprite.getY());
+    }
+
+    public void setPosition(float x, float y)
+    {
+        sprite.setPosition(x, y);
+        hitBox.setPosition(x, y);
+    }
+
+    public void draw(SpriteBatch batch)
+    {
+        sprite.draw(batch);
+    }
+
+    public void dispose()
+    {
+        texture.dispose();
+    }
+
+
+}
